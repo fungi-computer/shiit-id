2cb61b792b86e558a336e5af1ecd79be32361c78
liltechnomancer <levi@technomancy.dev>
2026-07-24 01:09:40 +0000

    Merge branch 'fix/workspace-id-lowercase'

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { customAlphabet } from "nanoid";
 
-export const ALPHABET_CROCKFORD =
-  "0123456789ABCDEFGHJKMNPQRSTVWXYZabcdefghjkmnpqrstvwxyz";
+export const ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 export const SHORT = 8;
 export const DEFAULT = 21;
 export const LONG = 32;
 
 export function nanoid(length: number = DEFAULT): string {
-  return customAlphabet(ALPHABET_CROCKFORD, length)();
+  return customAlphabet(ALPHABET, length)();
 }
